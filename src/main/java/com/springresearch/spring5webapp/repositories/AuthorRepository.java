a0f808964b7247247858646a4085156ac5788139
Your Name <you@example.com>
2020-06-20 07:33:05 -0500

    Revert "add repositories for CRUD support"
    This reverts commit fbc57a20

--- src/main/java/com/springresearch/spring5webapp/repositories/AuthorRepository.java
+++
@@ -1,7 +0,0 @@
-package com.springresearch.spring5webapp.repositories;
-
-import com.springresearch.spring5webapp.model.Author;
-import org.springframework.data.repository.CrudRepository;
-
-public interface AuthorRepository extends CrudRepository<Author, Long> {
-}
--- src/main/java/com/springresearch/spring5webapp/repositories/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.springresearch.spring5webapp.repositories;
-
-import com.springresearch.spring5webapp.model.Book;
-import org.springframework.data.repository.CrudRepository;
-
-public interface BookRepository extends CrudRepository<Book, Long> {
-}
