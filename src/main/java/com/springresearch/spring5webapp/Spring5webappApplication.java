9ef9492c63a6ab4afe6ac5f96956c951d0d702bd
Your Name <you@example.com>
2020-06-24 18:09:27 -0500

    add datasource

--- src/main/java/com/springresearch/spring5webapp/Spring5webappApplication.java
+++ src/main/java/com/springresearch/spring5webapp/Spring5webappApplication.java
@@ -4,6 +4,7 @@ import com.springresearch.spring5webapp.controllers.ConstructorInjectedControlle
 import com.springresearch.spring5webapp.controllers.MyController;
 import com.springresearch.spring5webapp.controllers.PropertyInjectedController;
 import com.springresearch.spring5webapp.controllers.SetterInjectedController;
+import com.springresearch.spring5webapp.examplebeans.FakeDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -17,10 +18,11 @@ public class Spring5webappApplication {
 		//create controller from the context bean.  Run method directly from controller.
 		MyController controller = (MyController) ctx.getBean("myController");
 
-		System.out.println(controller.hello());
-		System.out.println(ctx.getBean(PropertyInjectedController.class).sayHello());
-		System.out.println(ctx.getBean(SetterInjectedController.class).sayHello());
-		System.out.println(ctx.getBean(ConstructorInjectedController.class).sayHello());
+		FakeDataSource fakeDataSource = (FakeDataSource) ctx.getBean(FakeDataSource.class);
+
+		System.out.println(fakeDataSource.getUser());
+
+
 	}
 
 }
