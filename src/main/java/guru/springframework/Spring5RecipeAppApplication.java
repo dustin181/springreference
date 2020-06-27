41c19fb3ffa9aa7faec45de46472e2d384d818eb
Your Name <you@example.com>
2020-06-27 17:33:52 -0500

    add testing examples

--- src/main/java/guru/springframework/Spring5RecipeAppApplication.java
+++ src/main/java/guru/springframework/Spring5RecipeAppApplication.java
@@ -1,13 +1,12 @@
-package com.springresearch.spring5webapp;
+package guru.springframework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Spring5webappApplication {
+public class Spring5RecipeAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Spring5webappApplication.class, args);
+		SpringApplication.run(Spring5RecipeAppApplication.class, args);
 	}
-
 }
